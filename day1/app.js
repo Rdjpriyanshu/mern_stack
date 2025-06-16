7395bea424494d4697d1b721cd767683be17921f
Priyanshu <youremail@example.com>
2025-06-16 13:40:37 +0530

    first

---
+++ day1/app.js
@@ -0,0 +1,27 @@
+// window.console.log("Hello from JS!");
+
+// let , const OR var
+//  let age=30;
+//  const year=2010;
+//  var block=33;
+
+//  console.log(age,year,block);
+// console.log(age);
+
+// var age=22;  // declaration & initialisation
+// console.log(age);
+
+// var age = 21; // re-declaration
+// console.log(age);
+//  var block=22;
+//  console.log(age, year, block);
+
+// let age=29;
+// age=23
+// console.log(age);
+let a='b'
+let b=1;
+let c=a/b;
+console.log(c);
+// .............const
+
