e565fc25f400b81251712377a219166757743335
Priyanshu <youremail@example.com>
2025-06-25 14:13:50 +0530

    Css styling

--- day8/react-state-app/src/App.jsx
+++ day8/react-state-app/src/App.jsx
@@ -1,9 +1,11 @@
 import { Homepage } from "./pages/homepage";
 
-const App=()=>{
-return <div>
-  <Homepage/>
-</div>
-
+const App = () => {
+  return (
+    <div>
+      <Homepage />
+      <a>Priyanshu</a>
+    </div>
+  );
 };
-export default App;
+export default App ;
